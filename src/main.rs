d4d1eedc3e14c06b2f6b36d5cb0f877c597ef163
Ryan Greene <rabrg96@gmail.com>
2018-07-08 04:56:42 -0500

    Further documented main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
-#![feature(panic_implementation)]
-#![no_std]
-#![no_main]
+#![feature(panic_implementation)] // required for defining the panic handler
+#![no_std] // don't link the Rust standard library
+#![no_main] // disable all Rust-level entry points
 
 use core::panic::PanicInfo;
 
@@ -11,8 +11,9 @@ pub fn panic(_info: &PanicInfo) -> ! {
     loop {}
 }
 
-/// On Linux:
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named '_start' by default
     loop {}
 }
