3b8be66d5b6eb5d1f93e013a25c08d951196d881
Ryan Greene <rabrg96@gmail.com>
2018-07-08 13:08:28 -0500

    Created ColorCode structure in vga_buffer.rs

--- src/vga_buffer.rs
+++ src/vga_buffer.rs
@@ -19,3 +19,12 @@ pub enum Color {
     Yellow = 14,
     White = 15,
 }
+
+#[deprive(Debug, Clone, Copy, PartialEq, Eq)]
+struct ColorCode(u8);
+
+impl ColorCode {
+    fn new (foreground: Color, background: Color) -> ColorCode {
+        ColorCode((background as u8) << 4 | (foreground as u8))
+    }
+}
