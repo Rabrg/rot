a81044856cfa37d1a2d7a3e78c8f179c8c5bec1f
Ryan Greene <rabrg96@gmail.com>
2018-07-08 14:25:27 -0500

    Converted Buffer.chars to Volatile type

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@
 #![no_std] // don't link the Rust standard library
 #![no_main] // disable all Rust-level entry points
 
-mod vga_buffer;
+extern crate volatile;
 
 use core::panic::PanicInfo;
+
+mod vga_buffer;
 
 /// This function is called on panic.
 #[panic_implementation]
