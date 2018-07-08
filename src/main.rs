26424072fb268038f19089325522bc2f7803e9f7
Ryan Greene <rabrg96@gmail.com>
2018-07-08 03:42:33 -0500

    Made the Rust binary freestanding

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(panic_implementation)]
+#![no_std]
+#![no_main]
+
+use core::panic::PanicInfo;
+
+/// This function is called on panic.
+#[panic_implementation]
+#[no_mangle]
+pub fn panic(_info: &PanicInfo) -> ! {
+    loop {}
+}
+
+/// On Linux:
+#[no_mangle]
+pub extern "C" fn _start() -> ! {
+    loop {}
 }
